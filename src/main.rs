f8ddf7bb285844ee58614b73ca678e6122d2b927
Phudit Chuengjaroen <phudit.nut@gmail.com>
2025-03-01 23:23:24 +0700

    chore: move code to a new file

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,3 @@
 fn main() {
-    apply_operations(Vec::from([1,2,2,1,1,0,523,523]));
-}
-
-fn apply_operations(nums: Vec<i32>) -> Vec<i32> {
-    let mut temp: Vec<i32>  = nums.to_vec();
-    // Loop Apply Operation (Multiply).
-    let mut index = 0;
-    loop {
-        if temp[index] == temp[index+1] && temp[index] != 0 {
-            temp[index] = temp[index] * 2;
-            temp[index+1] = 0;
-        }
-
-        index+=1;
-
-        if index >= nums.len()-1 {
-            break;
-        }
-    }
-
-    // Loop shift zero.
-    let zero_vec: Vec<i32> = temp.to_vec().into_iter().filter(|&value| value == 0).collect();
-    let zero_count = zero_vec.len();
-    let mut normal_vec: Vec<i32> = temp.to_vec().into_iter().filter(|&value| value != 0).collect();
-    let mut fill_count = 1;
-
-    loop {
-        if fill_count > zero_count {
-            break;
-        }
-        normal_vec.push(0);
-        fill_count+=1;
-    }
-    return  normal_vec
+    // apply_operations(Vec::from([1,2,2,1,1,0,523,523]));
 }
