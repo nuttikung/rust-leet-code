a9ad6d072242dd5a7b98f70e0ce8bbcef5a02b8c
Phudit Chuengjaroen <phudit.nut@gmail.com>
2025-03-08 21:09:29 +0700

    feat: add minimum_recolors

---
+++ src/minimum_recolors.rs
@@ -0,0 +1,35 @@
+// println!("{}", minimum_recolors(String::from("WBBWWBBWBW") , 7))
+
+fn count_white(text: String) -> i32{
+    let mut white_count = 0;
+    for i in text.chars(){
+        if i == 'W' {
+            white_count+=1;
+        }
+    }
+    return white_count;
+}
+
+
+fn minimum_recolors(blocks: String, k: i32) -> i32 {
+    let max_window_len = blocks.len() as i32 - k;
+    let mut min = i32::MAX;
+    // sliding window
+    for index in 0..=max_window_len {
+        let start = index as usize;
+        let end = (index+k) as usize;
+        let sub_string = &blocks[start..end];
+        let count = count_white(String::from(sub_string));
+
+        if count == 0 {
+            min = count;
+            break;
+        }
+
+        if count < min {
+            min = count;
+        }
+    }
+
+    return min;
+}
